149263fc8154b269145cae54daf3bbffea930de6
vickyfrosties <sounia.mhalla@hotmail.com>
2024-10-28 13:52:59 +0100

    Lauching Page Design Finished + Buttons Link :computer:

--- src/Components/LauchingPage/LaunchingPage.jsx
+++ src/Components/LauchingPage/LaunchingPage.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from "./LauchingPage.module.css";
+
 const LaunchingPage = () => {
   return (
     <>
-      <img src="/Logo/StatMind - Logo.png" alt="StatMind Logo" />
-      <button>Sign In</button>
-      <button>Login</button>
+      <div className={styles.first_section}>
+        <img className={styles.logo} src="/Logo/StatMind - Logo.png" alt="StatMind Logo" />
+        <h3>StatMind</h3>
+        <h3>Reflect. Track. Grow.</h3>
+      </div>
+
+      <div className={styles.buttons_section}>
+        <Link className={styles.button} to="/register">Sign In</Link>
+        <Link className={styles.button} to="/login">Login</Link>
+
+      </div>
     </>
   );
 };
