098b9d4169122c216a1489375a4c407ee2bfa69b
vickyfrosties <sounia.mhalla@hotmail.com>
2024-10-29 17:55:44 +0100

    Launching, Sign In & Register Design Pages done :ok_hand:

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,19 +1,27 @@
-import styles from "./LoginForm.modules.css";
+import { Link } from "react-router-dom";
+import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
 
     return (
         <>
             <div className={styles.first_section}>
-
                 <img src="/Logo/StatMind - Logo.png" alt="StatMind Logo" />
                 <h3>StatMind</h3>
                 <h3>Reflect. Track. Grow.</h3>
             </div>
+
             <h3>Welcome back! Log in to continue.</h3>
-            <form action="">
-                <input type="text" name="Username" id="" />
+
+            <form className={styles.formulaire} action="">
+                <input type="text" name="Username" id="" placeholder="Username" />
+                <input type="text" name="Username" id="" placeholder="Password" />
             </form>
+            <Link className={styles.btn_container} to="/">
+                <button className={styles.register_btn}>Register</button>
+            </Link>
+
+            <p className={styles.p_login}>Don't have an account ? <span className={styles.signin_span}><Link to="/register">Sign In</Link></span></p>
         </>
     );
 };
