aedc5c00ff5cd0194a485a9f8d5a0397c8c1d6cd
vickyfrosties <sounia.mhalla@hotmail.com>
2024-11-02 18:44:54 +0100

    Problem not solved :neutral_face:

--- backend/Models/Users.js
+++ backend/Models/Users.js
@@ -1,13 +1,28 @@
 // this file is the model for mongoose which will indicates what types of data we want to fetch. 
 const mongoose = require("mongoose");
 
-const UsersSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true }
 });
 
-// compile a model from the schema to create a collection, here "members"
-const UsersModel = mongoose.model("members", UsersSchema);
-module.exports = UsersModel;
+// compile a model from the schema properties
+const Users = mongoose.model("Users", usersSchema);
+module.exports = Users;
 
+// export const bookSchema = new mongoose.Schema({
+//   title: String,
+//   serie: String,
+//   volume: Number,
+//   author: String,
+//   category: String,
+//   summary: String,
+//   finished: Boolean,
+//   opinion: Number,
+//   type: String,
+//   returned: Boolean,
+//   lent: Boolean,
+//   borrower: String
+// });
+// export const Books = mongoose.model("Books", bookSchema);
--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 // used for database modeling
 const mongoose = require("mongoose");
-const UsersModel = require("./Models/Users");
 const cors = require('cors');
+const Users = require("./Models/Users");
 const app = express();
 
 // autorize requests between cross-origin
@@ -17,8 +17,29 @@ mongoose.connect("mongodb://127.0.0.1:27017/users");
 // we're creating the route where we will get the request from and post it as a response from our database. This makes the link between front and backend.Post method
 app.post('/register', async (request, response) => {
   console.log('Received data:', request.body);
+
+  // async function createMember(body) {
+  //   const  =  await Books.create({
+  //       title : body.title,
+  //       serie:body.serie,
+  //       volume : body.volume,
+  //       author : body.author,
+  //       category : body.category,
+  //       summary : body.summary,
+  //       opinion : body.opinion,
+  //       finished: body.finished,
+  //       type : body.type,
+  //       returned: body.returned,
+  //       lent: body.lent,
+  //       borrower: body.borrower
+  //   })
+
+  //   const result = await newBook.save()
+  //   return result
+  // }
+
   try {
-    const member = new UsersModel(request.body);
+    const member = new Users(request.body);
     await member.save();
     response.send({ message: 'Member registered!' });
   }
