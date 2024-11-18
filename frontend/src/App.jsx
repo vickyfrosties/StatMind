f40df12578d026d6e74715df6ecf642a83eacbe4
Sounia <s.mhalla@interface3.be>
2024-11-18 16:11:37 +0100

    Dark Mode configuration :waxing_crescent_moon:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { createTheme, MantineProvider } from '@mantine/core';
+
 
 function App() {
 
+  const theme = createTheme({
+    primaryColor: "#202C31"
+  });
+
   return (
-    <>
+    <MantineProvider theme={theme}>
       <Outlet />
-    </>
+    </MantineProvider>
   );
 }
 
--- frontend/src/Components/HistoryPage/HistoryPage.jsx
+++ frontend/src/Components/HistoryPage/HistoryPage.jsx
@@ -2,22 +2,38 @@ import MainMenu from "../../Containers/Menu/MainMenu";
 import "/fonts.modules.css";
 import styles from "./History.module.css";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const HistoryPage = () => {
+  const [data, setData] = useState();
 
   const handleClick = async (e) => {
     e.preventDefault();
     const value = e.target.value;
     console.log(value);
-
-    await axios.get("http://localhost:8000/history")
-      .then(result => {
-        return console.log(result);
-      })
-      .catch(error => console.log("Error:", error));
-
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/history");
+        const data = response.data;
+        setData({
+          username: data.username,
+          emotions: data.emotions,
+          description: data.description,
+          favoriteMusic: data.favoriteMusic,
+          favoriteBook: data.favoriteBook,
+          pictureOfTheDay: data.pictureOfTheDay,
+          createdAt: data.createdAt
+        });
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    };
+    fetchData();
+  }
+    , []);
   return (
     <>
       <section className={styles.main_section}>
@@ -30,6 +46,7 @@ const HistoryPage = () => {
 
         <section className={styles.snd_section}>
           <div className={styles.date}>
+            <h3>18/11</h3>
           </div>
           <div className={styles.history}>
             <h3>
