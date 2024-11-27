cfcd5daa1828ec34c2bad9e833a0247b548ad943
vickyfrosties <sounia.mhalla@hotmail.com>
2024-11-27 22:36:04 +0100

    This calendar again...

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -9,7 +9,6 @@ import axios from "axios";
 
 const HomePage = () => {
   const username = localStorage.getItem("username");
-  const today = new Date().toLocaleDateString("fr-FR").slice(0, 5);
   const [date, setDate] = useState([]);
 
   useEffect(() => {
@@ -39,28 +38,28 @@ const HomePage = () => {
     return { startWeek, endWeek };
   }
 
-  const { startWeek, endWeek } = getCurrentWeek();
-
   function getWeekDays(starWeek) {
     const days = [];
     for (let i = 0; i < 7; i++) {
-      const day = new Date(starWeek);
-      day.setDate(startWeek.getDate() + i); // Add days to startOfWeek
+      const day = new Date(starWeek.getTime());
+      day.setDate(startWeek.getDate() + i);
       days.push(day);
     }
     return days;
   }
-  const weekDays = getWeekDays(startWeek);
 
   const emotionIcon = {
-    "Happy": "./Icons/p-happy.svg",
-    "Sad": "./Icons/p-sad.svg",
-    "Angry": "./Icons/p-angry.svg",
-    "Disgust": "./Icons/p-nervous.svg",
-    "Overwhelmed": "./Icons/p-melting.svg",
-    "Surprised": "./Icons/p-surprised.svg",
-    "Anxious": "./Icons/p-melting.svg",
+    Happy: { icon: "./Icons/p-happy.svg" },
+    Sad: { icon: "./Icons/p-sad.svg" },
+    Angry: { icon: "./Icons/p-angry.svg" },
+    Disgust: { icon: "./Icons/p-nervous.svg" },
+    Overwhelmed: { icon: "./Icons/p-melting.svg" },
+    Surprised: { icon: "./Icons/p-surprised.svg" },
+    Anxious: { icon: "./Icons/p-melting.svg" },
   };
+
+  const { startWeek } = getCurrentWeek();
+  const weekDays = getWeekDays(startWeek);
 
   return (
     <>
@@ -70,18 +69,32 @@ const HomePage = () => {
 
       <section className={styles.main_container}>
         <div className={styles.calendar_container} >
-          {weekDays.map((day) => (
-            <div key={day.toDateString()} className={styles.date_container}>
-              <p>{day.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase()}
-              </p>
 
-              <p>{day.toLocaleDateString('fr-FR', { day: 'numeric', month: 'numeric' })}
-              </p>
-              <img src={emotionIcon[date]} alt={date.emotions} />
+          {weekDays.map((day) => {
+            const dateString = day.toISOString().split("T")[0];
+            const emotion = date[dateString]?.emotion;
+            const today = new Date().toLocaleDateString("fr-FR");
+            const isToday = dateString === today;
+            const emotionData = emotion ? emotionIcon[emotion] : null;
 
-            </div>
-          ))}
+            return (
+              <div key={dateString} className={styles.date_container}>
+                <p>{day.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase()}</p>
+                <p>{day.toLocaleDateString('fr-FR', { day: 'numeric', month: 'numeric' })}</p>
+
+                {emotionData ? (
+                  <span>
+                    <img src={emotionData.icon} alt={emotion} />
+                  </span>
+                ) : (
+                  <span className={styles.no_data_dot}></span>
+                )}
+                {isToday && <span className={styles.today_dot}></span>}
+              </div>);
+          })}
         </div>
+
+
 
         <div className={styles.main}>
           <h2 className={styles.home_title}>Hi {username}, how do you feel today?</h2>
