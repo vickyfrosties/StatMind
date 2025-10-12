e93fb87e29fc6688565ef4346686a0df72c7cf4a
vickyfrosties <sounia.mhalla@hotmail.com>
2025-10-12 14:28:12 +0200

    chore: add .env file & .env.example to environment variable setup & import variable from .env to server

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const cron = require("node-cron");
@@ -8,14 +8,18 @@ const { statisticsController } = require("./controllers/dailyLog.controller");
 const cors = require("cors");
 const jwtTool = require("./tools/jwt.tool");
 // handles cookies session
-const session = require('express-session');
+const session = require("express-session");
 const authRouter = require("./routes/auth.route");
-const dailyLogRouter = require('./routes/dailyLog.route');
+const dailyLogRouter = require("./routes/dailyLog.route");
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+const serverPort = process.env.SERVER_PORT || 8000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // transfers the data in json
 app.use(express.json());
@@ -38,25 +42,25 @@ app.use((request, response, next) => {
   next();
 });
 
-app.use(session({
-  secret: process.env.JWT_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  // change to true if HTTPS request. Here HTTP ONLY
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    // change to true if HTTPS request. Here HTTP ONLY
+    cookie: { secure: false },
+  })
+);
 
 cron.schedule("0 0 0 * * *", () => {
   statisticsController();
-  console.log('Reset statistics at midnight.');
+  console.log("Reset statistics at midnight.");
 });
 
 // Routes
 app.use(authRouter);
 app.use(dailyLogRouter);
 
-app.listen(8000, () => {
-  console.log("Server is running");
-})
-
-
+app.listen(serverPort, () => {
+  console.log(`Server is running on PORT ${serverPort}`);
+});
