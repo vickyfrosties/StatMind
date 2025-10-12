2d99b09722c537d943834345a0e4d14adf85b14c
vickyfrosties <sounia.mhalla@hotmail.com>
2025-10-12 19:01:35 +0200

    refactor(backend): clean code on some controllers functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -10,14 +10,15 @@ mongoose.connect("mongodb://127.0.0.1/users");
 
 // Async request data from database
 async function registrationController(req, res) {
-
   try {
     const { username, email, password } = req.body;
 
     // check if the username already exist
     const existingMember = await Members.findOne({ username });
     if (existingMember) {
-      return res.status(400).json({ error: "Username already exist. Please choose another one." });
+      return res
+        .status(400)
+        .json({ error: "Username already exist. Please choose another one." });
     }
 
     // generate a salt. 10 is the default value
@@ -32,18 +33,23 @@ async function registrationController(req, res) {
     // generates the token
     const token = jwtTool.generate(data);
 
-    const newUser = await Members.create({ username, email, password: hash, authToken: token });
-    res.status(201).json({ message: "Account has been created successfully !", username: newUser });
-  }
-
-  catch (error) {
+    const newUser = await Members.create({
+      username,
+      email,
+      password: hash,
+      authToken: token,
+    });
+    res.status(201).json({
+      message: "Account has been created successfully !",
+      username: newUser,
+    });
+  } catch (error) {
     console.error("Error registering new member:", error);
     res.status(500).send("Error registering new member");
   }
 }
 
 async function loginController(req, res) {
-
   try {
     const { username, password } = req.body;
 
@@ -52,60 +58,66 @@ async function loginController(req, res) {
 
     // compare if the username & pwd stocked in the db are the same as the ones that the user is entering
     if (!memberCreds) {
-      return res.status(400).json({ error: "There's no account with this username. Please create an account." });
+      return res.status(400).json({
+        error:
+          "There's no account with this username. Please create an account.",
+      });
     }
     // using bcrypt.compare to compare the pwd in db and the one in input
     const isPwdValid = await bcrypt.compare(password, memberCreds.password);
 
     // if pwd is valid then operation is a success
     if (isPwdValid) {
-      return res.status(201).json({ success: "Credentials valid. Access authorized.", username: memberCreds.username });
+      return res.status(201).json({
+        success: "Credentials valid. Access authorized.",
+        username: memberCreds.username,
+      });
+    } else {
+      res.status(400).json({
+        error: "Username and password not correct. Please try again.",
+      });
     }
-
-    else {
-      res.status(400).json({ error: "Username and password not correct. Please try again." });
-    }
-  }
-
-  catch (error) {
+  } catch (error) {
     res.status(500).send("Error when member tries to connect", error);
     console.error("Error when member tries to connect:", error);
   }
-
 }
 
 async function logoutController(req, res) {
-
   req.session.destroy((error) => {
     if (error) {
-      return res.status(500).json({ error: "Error while trying to log out. Please try again." });
+      return res
+        .status(500)
+        .json({ error: "Error while trying to log out. Please try again." });
     }
 
     res.clearCookie("username");
     return res.status(200).json({ message: "You're logged out successfully!" });
   });
-
 }
 
 async function getHistoryData(req, res) {
   try {
     const username = req.query.username;
-    console.log('Received username:', username);
-    if (typeof username !== 'string') {
-      return res.status(400).json({ error: 'Invalid username format' });
+    if (typeof username !== "string") {
+      return res.status(400).json({ error: "Invalid username format" });
     }
     // find the data in the collection
     const data = await EmotionsData.find({ username })
-      // sort it as the greatest 
+      // sort it as the greatest
       .sort({ createdAt: -1 })
       // limit to 50 data
       .limit(20);
     res.json(data);
-  }
-  catch (error) {
-    console.error('Error fetching data from emotiondatas:', error);
-    res.status(500).json({ message: 'Failed to fetch data.' });
+  } catch (error) {
+    console.error("Error fetching data from emotiondatas:", error);
+    res.status(500).json({ message: "Failed to fetch data." });
   }
 }
 
-module.exports = { registrationController, loginController, logoutController, getHistoryData };
+module.exports = {
+  registrationController,
+  loginController,
+  logoutController,
+  getHistoryData,
+};
