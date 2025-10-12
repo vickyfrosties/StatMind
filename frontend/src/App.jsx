e93815ca88898911101a863d4fd8993639e6d38d
vickyfrosties <sounia.mhalla@hotmail.com>
2025-10-12 17:21:27 +0200

    feat: add Header & Footer to wrap App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import style from "./App.module.css";
+import Header from "./Containers/Header/Header";
+import Footer from "./Containers/Footer/Footer";
 
 function App() {
-
   return (
-    <Outlet />
+    <section className={style.app_container}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </section>
   );
 }
 
