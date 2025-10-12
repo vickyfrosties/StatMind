32aa0b137067ec485ba3d0998c1006c4f879a192
vickyfrosties <sounia.mhalla@hotmail.com>
2025-10-12 17:20:55 +0200

    feat: add navbar to navigate on devices other than mobile

--- frontend/src/Containers/Header/Header.jsx
+++ frontend/src/Containers/Header/Header.jsx
@@ -1,14 +1,35 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const username = localStorage.getItem("username");
 
   return (
     <>
-      <header className={styles.header}>
-        <div>
+      <header className={styles["header_container"]}>
+        <div className={styles["header_container_logo"]}>
           <img src="/Logo/StatMind - Logo.png" alt="StatMind Logo" />
           <h3>StatMind.</h3>
           <h3>Reflect. Track. Grow.</h3>
+        </div>
+        <nav className={styles["navbar"]}>
+          <ul>
+            <Link to="/home">
+              <p>Home</p>
+            </Link>
+            <Link to="/history">
+              <p>History</p>
+            </Link>
+            <Link to="/statistics">
+              <p>Statistics</p>
+            </Link>
+          </ul>
+        </nav>
+        <div className={styles["header_container_profile"]}>
+          <Link to="/profile">
+            {username ? <p> {username}'s account </p> : <p>My profile</p>}
+            <img src="/Icons/user.svg" alt="Profile Icon Page" />
+          </Link>
         </div>
       </header>
     </>
