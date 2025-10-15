a1dd6049dab13cd20b4f66736541c72ba2f93a1a
vickyfrosties <sounia.mhalla@hotmail.com>
2025-10-15 23:46:30 +0200

    feat: add conditional style to hide Footer whether it is /register or /login pages

--- frontend/src/Containers/Footer/Footer.jsx
+++ frontend/src/Containers/Footer/Footer.jsx
@@ -1,10 +1,16 @@
+import { useLocation } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
+  const location = useLocation();
+  const paths = ["/register", "/login"].includes(location.pathname);
 
   return (
     <>
-      <footer className={styles.footer}>
+      <footer
+        className={styles.footer}
+        style={paths ? { display: "none" } : { display: "block" }}
+      >
         <p>2024 - StatMind</p>
       </footer>
     </>
