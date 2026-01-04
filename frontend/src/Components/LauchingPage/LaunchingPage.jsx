0bdc8736bdd6d9884836e9c5bddbf7f96e16ad3f
vickyfrosties <sounia.mhalla@hotmail.com>
2026-01-04 21:40:41 +0100

    feat: change Launching page layout and add styles adjustements

--- frontend/src/Components/LauchingPage/LaunchingPage.jsx
+++ frontend/src/Components/LauchingPage/LaunchingPage.jsx
@@ -5,28 +5,29 @@ const LaunchingPage = () => {
   return (
     <>
       <div className={styles["first_section"]}>
-        <img
-          className={styles["logo"]}
-          src="./Logo/LogoWhiteV.png"
-          alt="StatMind Logo"
-        />
-
-        <div className={styles["buttons_section"]}>
-          <Link className={styles["button"]} to="/register">
-            Sign In
-          </Link>
-          <Link className={styles["button"]} to="/login">
-            Login
-          </Link>
+        <div className={styles["logo_section"]}>
+          <img src="/Logo/StatMindLogo.png" alt="Logo StatMind" />
+          <div>
+            <h3>StatMind.</h3>
+            <h3>Reflect. Track. Grow.</h3>
+          </div>
         </div>
-      </div>
 
-      <div className={styles["conditions"]}>
-        <p>
-          By continuing, you agree to the StatMind{" "}
-          <span className={styles["conditions_span"]}>Terms of Service</span>
-          and <span className={styles["conditions_span"]}>Privacy Policy</span>.
-        </p>
+        <div className={styles["authentification_section"]}>
+          <h2>
+            Welcome ! Get started with a new account or your existing StatMind
+            account.
+          </h2>
+
+          <div className={styles["buttons_section"]}>
+            <Link className={styles["button_register"]} to="/register">
+              Sign In
+            </Link>
+            <Link className={styles["button_log"]} to="/login">
+              Login
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
